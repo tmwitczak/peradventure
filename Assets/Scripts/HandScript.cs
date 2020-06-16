865f2f550beef90f01ff57e248442a61fdc463fd
precel120 <darkuso@o2.pl>
2020-06-16 22:18:40 +0200

    Hands spawning and movement

---
+++ Assets/Scripts/HandScript.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
+
+public class HandScript : MonoBehaviour
+{
+    public float Speed;
+
+    public GameObject Hive;
+
+    private bool moveBack;
+
+    private Vector3 initialPos;
+
+    private float destructionTimer = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveBack = false;
+        initialPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = Speed * Time.deltaTime;
+        if (!moveBack)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Hive.transform.position, step);
+            Vector3 direction = (Hive.transform.position - transform.position).normalized;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Quaternion lookRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, step * 6);
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, initialPos, step);
+            destructionTimer += Time.deltaTime;
+            if (destructionTimer >= 1.0f)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Hive"))
+        {
+            moveBack = true;
+        }
+    }
+}
---
+++ Assets/Scripts/HandSpawner.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class HandSpawner : MonoBehaviour
+{
+    public GameObject Hand;
+    private Vector3 screenMin;
+    private Vector3 screenMax;
+    private float spawnTimer = 0.0f;
+    private float spawnCooldown = 1.0f;
+    private List<float> spawnX = new List<float>();
+    private List<float> spawnY = new List<float>();
+    private int tmp = 0;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        screenMin = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
+        screenMax = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        for (float i = screenMin.x - 5.0f; i < screenMax.x + 5.0f; i+=0.5f)
+        {
+            spawnX.Add(i);
+        }
+        for (float i = screenMin.y - 2.0f; i < screenMax.y + 2.0f; i += 0.5f)
+        {
+            spawnY.Add(i);
+        }
+        spawnX = spawnX.Where(value => value <= screenMin.x || value >= screenMax.x).ToList();
+        spawnY = spawnY.Where(value => value <= screenMin.y || value >= screenMax.y).ToList();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        spawnTimer += Time.deltaTime;
+        if (spawnTimer >= spawnCooldown)
+        {
+            SpawnHand();
+            spawnTimer = 0.0f;
+        }
+    }
+
+    private void SpawnHand()
+    {
+        tmp += 1000;
+        Random.InitState(tmp);
+        var spawnPosition = new Vector2(spawnX[Random.Range(0, spawnX.Count)], spawnY[Random.Range(0, spawnY.Count)]);
+        Instantiate(Hand, spawnPosition, Quaternion.identity);
+    }
+}
