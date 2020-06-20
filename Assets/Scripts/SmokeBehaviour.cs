ecc8c53f601f9b755524c0e3a5f4d025aa2da3de
Adam <adamj500@interia.pl>
2020-06-20 21:27:46 +0200

    Modify smoke to work as intended
    Smoke is now emitted properly, not being moved
    Player can get rid of smoke by swiping

--- Assets/Scripts/SmokeBehaviour.cs
+++ Assets/Scripts/SmokeBehaviour.cs
@@ -2,60 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-enum SmokeType
-{
-    Up,
-    Down,
-    Right,
-    Left
-}
-
 public class SmokeBehaviour : MonoBehaviour
 {
-    [SerializeField] GameObject SmokeLeft;
-    [SerializeField] GameObject SmokeRight;
-    [SerializeField] GameObject SmokeUp;
-    [SerializeField] GameObject SmokeDown;
-
-    private Vector3 lSmokeStartPos;
-    private Vector3 rSmokeStartPos;
-    private Vector3 uSmokeStartPos;
-    private Vector3 dSmokeStartPos;
-
-    private Vector3 lSmokeEndPos;
-    private Vector3 rSmokeEndPos;
-    private Vector3 uSmokeEndPos;
-    private Vector3 dSmokeEndPos;
+    [SerializeField] ParticleSystem SmokeLeft;
+    [SerializeField] ParticleSystem SmokeRight;
+    [SerializeField] ParticleSystem SmokeUp;
+    [SerializeField] ParticleSystem SmokeDown;
 
     private float smokeTimer = 0.0f;
     private float smokeCooldown = 2.0f;
-    private float moveAmount = 10.0f;
-    private bool[] moved;
-    private bool[] startMovement;
+    private bool[] startPlaying;
     private float[] startTime;
+
+    private float EMITTING_TIME = 10.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        lSmokeStartPos = SmokeLeft.transform.position;
-        rSmokeStartPos = SmokeRight.transform.position;
-        uSmokeStartPos = SmokeUp.transform.position;
-        dSmokeStartPos = SmokeDown.transform.position;
-
-        lSmokeEndPos = SmokeLeft.transform.position + new Vector3(moveAmount, 0.0f, 0.0f);
-        rSmokeEndPos = SmokeRight.transform.position + new Vector3(-moveAmount, 0.0f, 0.0f);
-        uSmokeEndPos = SmokeUp.transform.position + new Vector3(0.0f, -moveAmount, 0.0f);
-        dSmokeEndPos = SmokeDown.transform.position + new Vector3(0.0f, moveAmount, 0.0f);
-
-        moved = new bool[4];
-        startMovement = new bool[4];
+        startPlaying = new bool[4];
         startTime = new float[4];
 
         for (int i = 0; i < 4; i++)
         {
             startTime[i] = 0.0f;
-            moved[i] = false;
-            startMovement[i] = false;
+            startPlaying[i] = false;
         }
     }
 
@@ -65,110 +35,88 @@ public class SmokeBehaviour : MonoBehaviour
         smokeTimer += Time.deltaTime;
         if (smokeTimer >= smokeCooldown)
         {
-            MoveSmoke((int)Random.Range(0, 4));
+            ActivateSmoke((int)Random.Range(0, 4));
             smokeTimer = 0.0f;
-            smokeCooldown = Random.Range(5, 10);
+            smokeCooldown = Random.Range(1, 10);
         }
 
-        if(!moved[0] && startMovement[0])
+        if (startPlaying[0])
         {
-            float distanceCovered = (Time.time - startTime[0]);
-            float distanceSmoothing = distanceCovered / moveAmount;
-            SmokeLeft.transform.position = Vector3.Lerp(lSmokeStartPos, lSmokeEndPos, distanceSmoothing);
-            if(SmokeLeft.transform.position.x == lSmokeEndPos.x)
+            if(SmokeLeft.isStopped)
             {
-                startMovement[0] = false;
-                moved[0] = true;
+                SmokeLeft.Play();
+            }
+            float timePlaying = (Time.time - startTime[0]);
+            if (timePlaying > EMITTING_TIME && SmokeLeft.isPlaying)
+            {
+                SmokeLeft.Stop();
+                startPlaying[0] = false;
             }
         }
 
-        if(!moved[1] && startMovement[1])
+        if (startPlaying[1])
         {
-            float distanceCovered = (Time.time - startTime[1]);
-            float distanceSmoothing = distanceCovered / moveAmount;
-            SmokeRight.transform.position = Vector3.Lerp(rSmokeStartPos, rSmokeEndPos, distanceSmoothing);
-            if (SmokeRight.transform.position.x == rSmokeEndPos.x)
+            if (SmokeRight.isStopped)
             {
-                startMovement[1] = false;
-                moved[1] = true;
+                SmokeRight.Play();
+            }
+            float timePlaying = (Time.time - startTime[0]);
+            if (timePlaying > EMITTING_TIME && SmokeRight.isPlaying)
+            {
+                SmokeRight.Stop();
+                startPlaying[1] = false;
             }
         }
 
-        if(!moved[2] && startMovement[2])
+        if (startPlaying[2])
         {
-            float distanceCovered = (Time.time - startTime[2]);
-            float distanceSmoothing = distanceCovered / moveAmount;
-            SmokeUp.transform.position = Vector3.Lerp(uSmokeStartPos, uSmokeEndPos, distanceSmoothing);
-            if (SmokeUp.transform.position.y == uSmokeEndPos.y)
+            if (SmokeUp.isStopped)
             {
-                startMovement[2] = false;
-                moved[2] = true;
+                SmokeUp.Play();
+            }
+            float timePlaying = (Time.time - startTime[0]);
+            if (timePlaying > EMITTING_TIME && SmokeUp.isPlaying)
+            {
+                SmokeUp.Stop();
+                startPlaying[2] = false;
             }
         }
-        
-        if(!moved[3] && startMovement[3])
+
+        if (startPlaying[3])
         {
-            float distanceCovered = (Time.time - startTime[3]);
-            float distanceSmoothing = distanceCovered / moveAmount;
-            SmokeDown.transform.position = Vector3.Lerp(dSmokeStartPos, dSmokeEndPos, distanceSmoothing);
-            if (SmokeDown.transform.position.y == dSmokeEndPos.y)
+            if (SmokeDown.isStopped)
             {
-                startMovement[3] = false;
-                moved[3] = true;
+                SmokeDown.Play();
+            }
+            float timePlaying = (Time.time - startTime[0]);
+            if (timePlaying > EMITTING_TIME && SmokeDown.isPlaying)
+            {
+                SmokeDown.Stop();
+                startPlaying[0] = false;
             }
         }
     }
 
-    void MoveSmoke(int i)
+    void ActivateSmoke(int i)
     {
-        switch(i)
+        Debug.Log(i);
+        switch (i)
         {
             case 0:
-                if(!moved[i])
-                {
-                    startTime[i] = Time.time;
-                    startMovement[i] = true;
-                }
+                startTime[i] = Time.time;
+                startPlaying[i] = true;
                 break;
             case 1:
-                if (!moved[i])
-                {
-                    startTime[i] = Time.time;
-                    startMovement[i] = true;
-                }
+                startTime[i] = Time.time;
+                startPlaying[i] = true;
                 break;
             case 2:
-                if (!moved[i])
-                {
-                    startTime[i] = Time.time;
-                    startMovement[i] = true;
-                }
+                startTime[i] = Time.time;
+                startPlaying[i] = true;
                 break;
             case 3:
-                if (!moved[i])
-                {
-                    startTime[i] = Time.time;
-                    startMovement[i] = true;
-                }
-                break;
-        }
-    }
-
-    private void ResetSmoke(SmokeType type)
-    {
-        switch(type)
-        {
-            case SmokeType.Up:
-                SmokeUp.transform.position = uSmokeStartPos;
-                break;
-            case SmokeType.Down:
-                SmokeDown.transform.position = dSmokeStartPos;
-                break;
-            case SmokeType.Right:
-                SmokeRight.transform.position = rSmokeStartPos;
-                break;
-            case SmokeType.Left:
-                SmokeLeft.transform.position = lSmokeStartPos;
+                startTime[i] = Time.time;
+                startPlaying[i] = true;
                 break;
         }
     }
