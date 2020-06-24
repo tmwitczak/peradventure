45ba1e73589201079afc66136c265db9986c8e05
Maciej <darkuso@o2.pl>
2020-06-24 11:40:34 +0200

    Collecting honey depending on amount of bees

---
+++ Assets/Scripts/BeesAmountScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BeesAmountScript : MonoBehaviour
+{
+    private Text text;
+    private List<GameObject> bees;
+    private int initialNumOfBees;
+    public int ActualNumOfBees
+    {
+        get { return actualNumOfBees; }
+    }
+    private int actualNumOfBees;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        initialNumOfBees = GameObject.FindGameObjectsWithTag("Bee").Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        actualNumOfBees = GameObject.FindGameObjectsWithTag("Bee").Length;
+        text.text = actualNumOfBees + "/" + initialNumOfBees;
+    }
+}
--- Assets/Scripts/HoneyBar.cs
+++ Assets/Scripts/HoneyBar.cs
@@ -3,15 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Object = System.Object;
 
 public class HoneyBar : MonoBehaviour
 {
     public Slider slider;
     public float Speed;
+    private BeesAmountScript beesAmountScript;
+    private int amountOfBees;
+
+    private void Start()
+    {
+        beesAmountScript = FindObjectOfType<BeesAmountScript>();
+    }
 
     private void Update()
     {
-        slider.value += Time.deltaTime * Speed;
+        amountOfBees = beesAmountScript.ActualNumOfBees;
+        slider.value += Time.deltaTime * amountOfBees * Speed;
     }
     public void setMaxHoneyAmount(float value)
     {
