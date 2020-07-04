b145736b37614200b69f21ec4fe0be0ff8c8ad1f
Adam <adamj500@interia.pl>
2020-07-04 16:48:40 +0200

    Add counter for on screen smoke amount

---
+++ Assets/Scripts/SmokeCounter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SmokeCounter : MonoBehaviour
+{
+    private HoneyCounter honeyCounter;
+    private Text text;
+    private float smokeAmount = 0f;
+    private string initialText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        honeyCounter = FindObjectOfType<HoneyCounter>();
+        text = GetComponent<Text>();
+        initialText = text.text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = initialText + ComputeSmokeScreen() + " %";
+    }
+
+    float ComputeSmokeScreen()
+    {
+        smokeAmount = 1 - (Mathf.Round(honeyCounter.getSmokeFactor() * 100f) / 100f);
+        return (Mathf.Round(smokeAmount * 10f) / 2f) * 10f;
+    }
+}
