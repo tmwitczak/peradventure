795d1c79e9b18e915529f3c1a00e4d01fb997bd7
Tomasz Witczak <TomaszMWitczak@gmail.com>
2020-12-04 21:21:59 +0100

    Remove unnecessary import

--- Assets/Scripts/BladeScript.cs
+++ Assets/Scripts/BladeScript.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using UnityEditor.XR;
 using UnityEngine;
 
 public class BladeScript : MonoBehaviour
