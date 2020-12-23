b88ae38b2f855e25c80a712a1baf848f44874e74
Tomasz Witczak <TomaszMWitczak@gmail.com>
2020-12-23 02:57:14 +0100

    Optimize post-processing and background rendering (#176243381)
    - Render background to texture
    - Disable redundant environment objects after rendering to texture
    - Adjust post-processing settings
    - Rewrite camera structure

---
+++ Assets/Scripts/PreRenderBackground.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PreRenderBackground : MonoBehaviour {
+    private int frames = 0;
+    private void Update() {
+        // Disable after two frames
+        if (frames <= 1) {
+            frames++;
+        } else {
+            gameObject.SetActive(false);
+            GameObject.Find("Environment").SetActive(false);
+        }
+    }
+}
