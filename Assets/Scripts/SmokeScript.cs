b9dd1ec9dd3553185431de2a6c14c4ecce0c597d
Adam <adamj500@interia.pl>
2021-01-14 19:38:31 +0100

    Fix bees not speeding up after cleaning the smoke

--- Assets/Scripts/SmokeScript.cs
+++ Assets/Scripts/SmokeScript.cs
@@ -9,24 +9,34 @@ public class SmokeScript : MonoBehaviour
     private float smokeEffect = 0.003f;
     private ParticleSystem particleSystem;
     List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
+    private int particlesTriggered = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         particleSystem = GetComponent<ParticleSystem>();
     }
 
+    private void Update()
+    {
+        if(particlesTriggered == 0)
+        {
+            honeyCounter.setSmokeFactor(honeyCounter.getSmokeFactor() + smokeEffect / 100.0f);
+        }
+    }
+
     private void OnParticleCollision(GameObject other)
     {
         if (other.CompareTag("Blade"))
         {
-            honeyCounter.setSmokeFactor(honeyCounter.getSmokeFactor() + smokeEffect);
+            honeyCounter.setSmokeFactor(honeyCounter.getSmokeFactor() + smokeEffect * 2.0f);
         }
     }
 
     private void OnParticleTrigger()
     {
-        int numEnter = particleSystem.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
-        for (int i = 0; i < numEnter; i++)
+        particlesTriggered = particleSystem.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+        for (int i = 0; i < particlesTriggered; i++)
         {
             honeyCounter.setSmokeFactor(honeyCounter.getSmokeFactor() - smokeEffect);
         }
