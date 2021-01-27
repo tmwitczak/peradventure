dc1cb46b827f0566af7d6ffa82ffbb82bbec8e9d
Tomasz Witczak <TomaszMWitczak@gmail.com>
2021-01-27 15:17:37 +0100

    Redesign the stopwatch
    - Fix the bug with the stopwatch hand not rotating in sync with the fill
      (#176629412). The issue was caused by the fillAmount property not
      really operating on angles (percentage of 360 degrees fill) but on the
      angular percentage of the image area. That way, when the chosen image
      is not square-shaped, the percentage of area filled does not match the
      angular percentage.
    - Change the colors of the watch to match the ones of the honey jar
      (honey counter) more closely.
    - Replace the watch hand with the honey dipper image (#176451416)
    - Implement a formula for dynamic scaling of the hand depending on the
      turn angle so that the object doesn't protrude from the clock face.
      Later it turned out that the formula isn't needed in favour of setting
      up the mask on the parent object but it's left here anyway in case of
      further need.
    - Make all the hexagon images preserve their original aspect ratio.
    - Decrease the line spacing of the endgame texts when spanning over
      multiple lines.

--- Assets/Scripts/StopwatchScript.cs
+++ Assets/Scripts/StopwatchScript.cs
@@ -1,43 +1,53 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class StopwatchScript : MonoBehaviour
-{
+public class StopwatchScript : MonoBehaviour {
     public static float MaxTime = 30.0f;
     public Transform clockHandTransform;
+    public Transform clockHandStick;
+    private Vector3 clockHandStickOriginalScale;
     [SerializeField] GameObject EndGameMenu;
     [SerializeField] GameObject Fill;
-    
+
     public static float timer = 0.0f;
     private float timeDuration;
     private HoneyCounter honeyCounter;
 
     public bool isTimerFinished = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        timeDuration = MaxTime;
-        honeyCounter = FindObjectOfType<HoneyCounter>();
+    private float fmod(float a, float b) {
+        return a - b * Mathf.Floor(a / b);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (timer < 1f)
-        {
+    private float polygonRadius(float angle, float sides, float scale) {
+        float alpha = -((sides - 2) * Mathf.PI / (2 * sides));
+        float beta = 2 * Mathf.PI / sides;
+        float a = Mathf.Tan(alpha);
+        float b = fmod(angle, beta);
+        return (-scale * a) / (Mathf.Sin(b) - a * Mathf.Cos(b));
+    }
+
+    void Start() {
+        timeDuration = MaxTime;
+        honeyCounter = FindObjectOfType<HoneyCounter>();
+        clockHandStickOriginalScale = clockHandStick.localScale;
+    }
+
+    void Update() {
+        if (timer < 1f) {
             timer += Time.deltaTime / timeDuration;
             float timeNormalized = timer % 1f;
             clockHandTransform.eulerAngles = new Vector3(0, 0, -timeNormalized * 360f);
+            clockHandStick.localScale = new Vector3(
+                    clockHandStickOriginalScale.x,
+                    polygonRadius(timeNormalized * 2 * Mathf.PI, 6, clockHandStickOriginalScale.y),
+                    clockHandStickOriginalScale.z);
             Fill.GetComponent<Image>().fillAmount = timeNormalized;
-        }
-        else
-        {
+        } else {
             timer = 1f;
         }
 
-        if (timer == 1f && !isTimerFinished)
-        {
+        if (timer == 1f && !isTimerFinished) {
             honeyCounter.endHoneyAmount = honeyCounter.HoneyAmount;
             EndGameMenu.SetActive(true);
             HiveLevel.resultsActive = true;
@@ -47,8 +57,7 @@ public class StopwatchScript : MonoBehaviour
         }
     }
 
-    public void resetStopwatch()
-    {
+    public void resetStopwatch() {
         timer = 0.0f;
         Fill.GetComponent<Image>().fillAmount = 0.0f;
         isTimerFinished = false;
