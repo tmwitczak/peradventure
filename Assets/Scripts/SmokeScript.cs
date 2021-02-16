37b28e1c9089119c3624439c34641f29e23240a1
Adam <adamj500@interia.pl>
2021-02-16 23:36:24 +0100

    Edit smoke script for better bee speed modifying

--- Assets/Scripts/SmokeScript.cs
+++ Assets/Scripts/SmokeScript.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class SmokeScript : MonoBehaviour {
     List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
     private new ParticleSystem particleSystem;
-    private float smokeEffect = 0.003f;
-    private float recoverySpeed = 30.0f;
+    private float smokeEffect = 0.005f;
+    private float recoverySpeed = 10f;
+    private float bladeRecovery = 1.5f;
     private int particlesTriggered = 0;
     public HoneyCounter honeyCounter;
 
@@ -15,17 +16,37 @@ public class SmokeScript : MonoBehaviour {
     }
 
     private void Update() {
-        honeyCounter.SmokeFactor +=
-            Convert.ToSingle(particlesTriggered == 0) * smokeEffect / recoverySpeed;
+        honeyCounter.SmokeFactor += (smokeEffect / recoverySpeed) * particleMultiplier();
     }
 
     private void OnParticleCollision(GameObject other) {
         honeyCounter.SmokeFactor +=
-            Convert.ToSingle(other.CompareTag("Blade")) * smokeEffect * 2.0f;
+            Convert.ToSingle(other.CompareTag("Blade")) * smokeEffect * bladeRecovery;
     }
 
     private void OnParticleTrigger() {
         particlesTriggered = particleSystem.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
         honeyCounter.SmokeFactor -= particlesTriggered * smokeEffect;
+    }
+
+    private float particleMultiplier()
+    {
+        float particleMultiplier = 0.0f;
+        switch (particlesTriggered)
+        {
+            case 0:
+                particleMultiplier = 1.0f;
+                break;
+            case 1:
+                particleMultiplier = 0.2f;
+                break;
+            case 2:
+                particleMultiplier = 0.1f;
+                break;
+            default:
+                particleMultiplier = 0.0f;
+                break;
+        }
+        return particleMultiplier;
     }
 }
