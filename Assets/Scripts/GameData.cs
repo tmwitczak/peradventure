02267c349be94b94131509082994c47ee0969913
Adam <adamj500@interia.pl>
2021-02-18 18:19:11 +0100

    Add appVersion parameter to GameData

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -1,5 +1,6 @@
 ﻿[System.Serializable]
 public class GameData {
+    public string _appVersion = "0.0.0";
     public float _honeyAmount = 0;
     public float _levelMaxValue = 100;
     public int _amountOfBees = 30;
